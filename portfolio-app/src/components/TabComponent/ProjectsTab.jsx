41846207999fef7dc63c3400498ec610f0db3673
Aaron Ramirez <55257824+aram249@users.noreply.github.com>
2023-09-19 22:11:59 -0700

    Progress on portfolio page
    made progress on portfolio tab.

--- portfolio-app/src/components/TabComponent/ProjectsTab.jsx
+++ portfolio-app/src/components/TabComponent/ProjectsTab.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import '../../styles/Card.css'
+import '../../styles/Portfolio.css'
 
 const ProjectsTab = (props) => {
     const projects = props.projects;
 
     return (
-        <div className='projects'>
+        <div className='portfolio--section--container'>
             <>        
                 {projects.map((value, index) =>(
-                    <div className="card" key={index}>
-                            <h2 className='title'>
-                                {value.projectName}
-                            </h2>
-                            <p className='description'>
-                                {value.description}
+                    <div className="portfolio--section--card" key={index}>
+                        <div className='portfolio--section--img'>
+                            <img src={value.img} alt='placeholder'/>
+                        </div>
+                        <div className='portfolio--section--car--content'>
+                            <div>
+                                <h3 className='portfolio--section-title'>
+                                    {value.projectName}
+                                </h3>
+                                <p className='text-md'>
+                                    {value.description}
+                                </p>
+                            </div>
+                            <p className='text-sm-portfolio--link'>
+                            {/* link will go here to link to github */}
                             </p>
+                        </div>
                     </div>
                 ))}
             </>
